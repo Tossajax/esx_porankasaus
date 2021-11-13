82dd270143e0b95f4268402156557973a8fa0147
Tossajax <91850652+Tossajax@users.noreply.github.com>
2021-11-13 20:21:53 +0200

    Update config.lua

--- esx_porankasaus/config.lua
+++ esx_porankasaus/config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.paskat = {
 	["kasauspaikka"] = {
-		[1] = { ["x"] = 1417.16, ["y"] = 6339.27, ["z"] = 24.4 } 
+		[1] = { ["x"] = 0.0, ["y"] = 0.0, ["z"] = 0.0 } --laita omat coordinaatit tähä ja clienttii
 	}
 }
