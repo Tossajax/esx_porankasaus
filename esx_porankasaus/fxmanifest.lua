4782d6e8ba238aec8a5e87dbc02fcbbd38558409
Tossajax <91850652+Tossajax@users.noreply.github.com>
2022-01-20 21:37:44 +0200

    Update fxmanifest.lua

--- esx_porankasaus/fxmanifest.lua
+++ esx_porankasaus/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'adamant'
 game 'gta5'
 
-client_scripts { "c/cl_*.lua", "config.lua" }
-server_scripts { "@mysql-async/lib/MySQL.lua", "s/sv_*.lua", "config.lua" }
+client_scripts { "c/cl_*.lua" }
+server_scripts { "@mysql-async/lib/MySQL.lua", "s/sv_*.lua" }
 
 author 'tossaja'
 version '1.0.0'
